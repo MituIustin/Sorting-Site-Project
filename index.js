a186a781f9d7445f86d5b724513e36401db25a43
MituIustin <mituiustin1708@gmail.com>
2023-05-07 09:12:20 +0300

    tema 2

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ window.addEventListener('beforeunload', function(event) {
   localStorage.clear();
 });
 
+const titlu = document.querySelector(".tit");
+const computedStyle = getComputedStyle(titlu);
+const font_family = computedStyle.fontFamily;
+const font_color = computedStyle.color;
+
+const paragraf1 = document.getElementsByClassName("p");
+
+for (let i = 0; i < paragraf1.length; i++) {
+  paragraf1[i].style.fontFamily = font_family;
+  paragraf1[i].style.color = font_color;
+}
