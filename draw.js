73f350526304a995c1977cd2f01b865dcac46193
MituIustin <mituiustin1708@gmail.com>
2023-05-07 10:20:24 +0300

    tema 2

---
+++ draw.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("canvas");
+canvas.style.width = "600px";
+canvas.style.height= "400px";
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#000000";
+ctx.font = "bold 20px sans-serif";
+
+ctx.fillText("         _,.,.__,--.__,-----.", 10, 20);
+ctx.fillText("       ,''   '))                 `.", 10, 40);
+ctx.fillText("     ,'   e                      ))", 10, 60);
+ctx.fillText("    (  .-'__,                    ,", 10, 80);
+ctx.fillText("     `~`    `-\  /._____,/   /", 10, 100);
+ctx.fillText("              | | )       (   (   ;    ", 10, 120);
+ctx.fillText("              | | |       /  / /  /", 10, 140);
+
+
+
